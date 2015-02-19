fbe9c49956de1b685354a1d1291504fad0806f46
Nathaniel Kofalt <nathaniel@kofalt.com>
2015-02-19 12:30:44 -0800

    Include bin folder in distributions

--- scripts/make-release.sh
+++ scripts/make-release.sh
@@ -5,7 +5,9 @@ cd "`git rev-parse --show-toplevel`"
 
 # Whitelist files to ship (AKA, don't put random certs + config in dist)
 files=(`git ls-files`)
+files+=("bin")
 files+=("code")
 files+=(`ls containers/*.tar*`)
 
+rm -f release.tar
 tar -cf release.tar ${files[@]}
