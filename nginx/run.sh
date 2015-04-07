fe37dbf213978b954fc7f3e157026bf9b10a5073
Kevin S. Hahn <kevinshahn@gmail.com>
2015-04-07 15:56:51 -0700

    overwrites custom ca-bundle every instance start
    instead of appending to the same file repeatedly, resulting in
    duplicates in the nginx/ca-certificates+scitranCA.crt file

--- nginx/run.sh
+++ nginx/run.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 # combine the ca-certificates bundle with out our trusted CA certificate
-cat /etc/ssl/certs/ca-certificates.crt /etc/nginx/rootCA-cert.pem >> /etc/nginx/ca-certificates+scitranCA.crt
+cat /etc/ssl/certs/ca-certificates.crt /etc/nginx/rootCA-cert.pem > /etc/nginx/ca-certificates+scitranCA.crt
 
 # start
 nginx $@
