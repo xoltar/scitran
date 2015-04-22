b09165aa084e1221e153ab1ddddc155a503a18ba
Kevin S. Hahn <kevinshahn@gmail.com>
2015-04-22 15:56:47 -0700

    specifies a certain dcmstack commit to install

--- api/build.sh
+++ api/build.sh
@@ -14,7 +14,7 @@ pip install numpy==1.9.0 \
 	pytz \
 	git+https://github.com/scitran/pydicom.git@0.9.9_value_vr_mismatch \
 	git+https://github.com/nipy/nibabel.git \
-	git+https://github.com/moloney/dcmstack.git \
+	git+https://github.com/moloney/dcmstack.git@6d49fe01235c08ae63c76fa2f3943b49c9b9832d \
 	uwsgi
 
 # Cleanup
